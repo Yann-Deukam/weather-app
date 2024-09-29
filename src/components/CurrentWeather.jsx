c69c8652ea2986cf9c9f1b4a28ec72aaac684b98
Yann-Donald <yanndonald237@gmail.com>
2024-09-29 05:43:50 +0100

    added realtime functionality, remaining to fix the UI and responsive

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const CurrentWeather = ({ currentWeather }) => {
-  const time = currentWeather.time
-    ? currentWeather.time.split(" ")[1].substring(0, 5)
-    : " ";
+  const [localTime, setLocalTime] = useState("");
+
+  useEffect(() => {
+    if (currentWeather.time) {
+      const [date, time] = currentWeather.time.split(" ");
+      const [year, month, day] = date.split("-").map(Number);
+      const [hour, minute] = time.split(":").map(Number);
+
+      let locationTime = new Date(year, month - 1, day, hour, minute);
+
+      // Function to update local time
+      const updateLocalTime = () => {
+        locationTime.setSeconds(locationTime.getSeconds() + 1);
+        const formattedTime = locationTime.toLocaleTimeString("en-GB", {
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        });
+        setLocalTime(formattedTime);
+      };
+
+      // Update local time immediately and set interval
+      updateLocalTime();
+      const interval = setInterval(updateLocalTime, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [currentWeather.time]);
+
+  // Function to format time based on screen size
+  const formatTimeForDisplay = () => {
+    const [hour, minute] = localTime.split(":");
+    return window.innerWidth < 768 // Assuming 768px as the breakpoint
+      ? `${hour}:${minute}` // Remove seconds for small screens
+      : localTime; // Show full time for larger screens
+  };
+
   return (
     <div className="current-weather-time flex">
       <div className="current-weather">
-        {/* <img src="icons/clouds.svg" className="weather-icon" /> */}
         <img
           src={`icon/${currentWeather.weatherIcon}.svg`}
           className="weather-icon h-16 mx-auto text-teal-400"
@@ -24,7 +57,9 @@ const CurrentWeather = ({ currentWeather }) => {
       <div className="current-time">
         <div className="relative pt-12">
           <img src="./icon/watch.svg" alt="watch" className="parameter-icon" />
-          <h2 className="text-4xl font-square text-teal-200">{time}</h2>
+          <h2 className="text-4xl font-square text-teal-200">
+            {formatTimeForDisplay()}
+          </h2>
         </div>
       </div>
     </div>
